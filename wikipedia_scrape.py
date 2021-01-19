a3b15817e247bba55b5843dc90010b1bd3a5f4ab
Matthew Childs <76403824+mcchilds01@users.noreply.github.com>
2021-01-18 20:49:19 -0500

    Add files via upload

---
+++ wikipedia_scrape.py
@@ -0,0 +1,44 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36\
+ (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+ 'from': 'mchilds3@vols.utk.edu'}
+
+rows = []
+
+url = 'https://www.wikipedia.org/wiki/Category:Women_computer_scientists'
+
+def scrapePage(url: str):
+    page = requests.get(url, headers=headers) 
+    page_content = page.content
+    soup = BeautifulSoup(page_content, 'html.parser')
+    content = soup.find('div', class_="mw-category")
+    page_goupings = content.find_all('div', class_='mw-category-group')
+
+    for grouping in page_goupings:
+        names_list = grouping.find('ul')
+        category = grouping.find('h3').get_text()
+        alphabetical_names = names_list.find_all('li')
+
+        for alphabetical_name in alphabetical_names:
+            name = alphabetical_name.text
+            anchortag = alphabetical_name.find('a', href=True)
+            link = anchortag['href']
+            letter_name = category
+            row = {
+                'name': name,
+                'link': link,
+                'letter_name':letter_name
+            }
+            rows.append(row)
+
+scrapePage(url)
+
+with open('women_computer_scientists.csv', 'w') as f:
+    fieldnames = ['name', 'link', 'letter_name']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in rows:
+        writer.writerow(row)
